5af7cf18a18cf574344a61687751cd871c149ade
sanmati-rm <sanmati-rm@users.noreply.github.com>
2020-03-14 22:47:18 +0530

    done

--- src/compare/compare.java
+++ src/compare/compare.java
@@ -10,10 +10,19 @@ public class compare {
         a=in.nextInt();
         b=in.nextInt();
         c=in.nextInt();
+        System.out.println("a = " + a + "b= " + b + "c= "+ c);
         if(a>b && a>c)
             System.out.println("a is largest");
         else if(b>a && b>c)
-            System.out.println("b");
-        
+            System.out.println("b is largest");
+        else
+            System.out.println("c is largest");
+
+        if((a>b && a<c) || (a>c && a<b))
+            System.out.println("a is second largest");
+        else if((b>a && b<c) || (b>c && b<a))
+            System.out.println("b is second largest");
+        else
+            System.out.println("c is second largest");
     }
 }
