8bd1aa221b7ab3162d0b6a28465c42dde3d2641f
sanmati-rm <sanmati-rm@users.noreply.github.com>
2020-03-14 20:45:03 +0530

    done

---
+++ src/swap2/swap2.java
@@ -0,0 +1,20 @@
+package swap2;
+
+import java.util.Scanner;
+
+public class swap2 {
+    public static void main(String[] args) {
+        int a,b,temp=0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number");
+        a=in.nextInt();
+        System.out.println("Enter another number");
+        b=in.nextInt();
+        System.out.println("The numbers before swapping are a=" + a + "b=" + b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("The numbers before swapping are a=" + a + "b=" + b);
+
+    }
+}
---
+++ src/swap3/swap3.java
@@ -0,0 +1,23 @@
+package swap3;
+
+import java.util.Scanner;
+
+public class swap3
+{
+    public static void main(String[] args)
+    {
+        int a,b,c,temp1=0,temp2=0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter 3 numbers\n");
+        a=in.nextInt();
+        b=in.nextInt();
+        c=in.nextInt();
+        System.out.println("The numbers before swapping are a=" + a + "\t" + "b=" + b + "\t" +  "c=" + c);
+        temp1=a;
+        a=b;
+        temp2=b;
+        b=c;
+        c=temp1;
+        System.out.println("The numbers before swapping are a=" + a + "\t" + "b=" + b + "\t" + "c=" + c);
+    }
+}
